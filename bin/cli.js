07e821c6c98b3827f00ce681a161f077afbe16aa
Jatin Sahijwani <117177197+jatinsahijwani@users.noreply.github.com>
2025-10-19 03:36:47 +0530

    Feat/add testing module (#25)
    * feat(test): add initial test.js module for CLI testing
    
    * feat(cli): integrate test command into CLI entrypoint

--- bin/cli.js
+++ bin/cli.js
@@ -13,6 +13,8 @@
 
 const { program } = require('commander');
 const {compileCircuit} = require('../lib/compile');
+const {testCircuit} = require('../lib/test');
+
 /**
  * Define the `compile` command
  * ----------------------------
@@ -34,8 +36,15 @@ program
   .command('compile <circomFilePath>')
   .description('Compile a circom circuit')
   .action((circomFilePath) => {
-    // TODO: Call compile function here once implemented.
     compileCircuit(circomFilePath);
+  });
+
+
+  program
+  .command('test <folder> <inputJson>')
+  .description('Test the circuit with input.json and generate proof/public.json')
+  .action((folder, inputJson) => {
+    testCircuit(folder, inputJson);
   });
 
 /**
